371759cc12441e41662b00a875a608cd15c7c9c4
malewis5 <matthew.a.lewis7@gmail.com>
2024-08-19 14:58:59 -0400

    diff api

--- app/api/time/route.ts
+++
@@ -1,6 +0,0 @@
-export function GET() {
-  const currentTime = new Date().toISOString();
-  return Response.json({ currentTime });
-}
-
-export const dynamic = "force-dynamic";
--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,28 @@ import { getUserId } from "../lib/auth";
 export default async function Home() {
   const user = await getUserId();
 
-  const time = await fetch("http://localhost:3000/api/time", {
-    cache: !!user ? "no-store" : "default",
-  }).then((res) => res.json());
+  const res = await fetch(
+    `https://timeapi.io/api/time/current/zone?timeZone=Europe%2FAmsterdam
+`,
+    {
+      cache: !!user ? "no-store" : "default",
+    }
+  )
+    .then((res) => {
+      if (!res.ok) {
+        console.log(res);
+        throw new Error("Network response was not ok");
+      }
+      return res.json();
+    })
+    .catch(() => {
+      return { datetime: "error" };
+    });
 
   return (
     <>
       <p>draft mode is: {draftMode().isEnabled.toString()}</p>
-      <p>fetch response: {time.currentTime}</p>
+      <p>fetch response: {res.dateTime}</p>
     </>
   );
 }
